575cb9c36c00a90dcd2c6ba35d16a33af18e5fe2
Leonardo Ribas de Souza <leonardodsouza@gmail.com>
2017-11-16 18:46:16 -0200

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     @backgound_1 = Banner.find(1)
     @backgound_2 = Banner.find(2)
     @backgound_3 = Banner.find(3)
+    @backgound_7 = Banner.find(7)
     @pagina_1 = Pagina.find(1)
     @pagina_2 = Pagina.find(2)
 
