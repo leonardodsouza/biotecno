8096b619bebf41861546db5a14522f454cb48090
Leonardo Ribas de Souza <leonardodsouza@gmail.com>
2017-11-27 16:12:18 -0200

    commit

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,8 @@
+CKEDITOR.editorConfig = function( config )
+{
+	config.toolbar = 'MyToolbar';
+	config.uiColor = '#FFFFFF';
+	config.removePlugins = 'elementspath';
+	config.resize_enabled = false;
+	
+};
